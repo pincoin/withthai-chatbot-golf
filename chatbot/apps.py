52d70e1c71d5ff30708cd0e849d4d17152a83bc8
jonghwa <pincoins@gmail.com>
2020-08-05 18:16:22 +0900

    apps meta

--- chatbot/apps.py
+++ chatbot/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class ChatbotConfig(AppConfig):
     name = 'chatbot'
+    verbose_name = _('chatbot')
---
+++ golf/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'golf.apps.GolfConfig'
