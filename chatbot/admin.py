e6cd9f041468710b3989e9c74f1cad3d025156c0
jonghwa <pincoins@gmail.com>
2020-08-06 03:18:21 +0900

    webhook logs

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -4,7 +4,10 @@ from . import models
 
 
 class WebhookLogAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('created',)
+    search_fields = ('request_body', 'response_body')
+    readonly_fields = ('request_header', 'request_body', 'response_header', 'response_body', 'created')
+    ordering = ('-created',)
 
 
 admin.site.register(models.WebhookLog, WebhookLogAdmin)
