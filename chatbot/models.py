fb4ecc82e08cf222f75b5fd261d6d7d915dc01ea
jonghwa <pincoins@gmail.com>
2020-08-09 02:34:37 +0900

    remove models

--- chatbot/models.py
+++ chatbot/models.py
@@ -20,21 +20,3 @@ class WebhookRequestLog(model_utils_models.TimeStampedModel):
 
     def __str__(self):
         return '{}'.format(self.created)
-
-
-class EventLog(model_utils_models.TimeStampedModel):
-    class Meta:
-        verbose_name = _('Event log')
-        verbose_name_plural = _('Event logs')
-
-    def __str__(self):
-        return '{}'.format(self.created)
-
-
-class MessageLog(model_utils_models.TimeStampedModel):
-    class Meta:
-        verbose_name = _('Message log')
-        verbose_name_plural = _('Message logs')
-
-    def __str__(self):
-        return '{}'.format(self.created)
