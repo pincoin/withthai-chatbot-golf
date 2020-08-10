5ff33e2ff3ae56b46112ca3acd0d475e76c1e790
jonghwa <pincoins@gmail.com>
2020-08-10 23:03:22 +0900

    remove get profile

--- chatbot/static/js/chatbot/liff/liff.js
+++ chatbot/static/js/chatbot/liff/liff.js
@@ -1,12 +1,11 @@
 window.onload = function (e) {
-    // init で初期化。基本情報を取得。
-    // https://developers.line.me/ja/reference/liff/#initialize-liff-app
+    const liffId = '1654038916-kOX5RXl4'
+    // https://developers.line.biz/en/reference/liff/#initialize-liff-app
     liff.init({
-        liffId: '1654038916-kOX5RXl4'
+        liffId: liffId
     }).then(() => {
         getProfile();
-        // start to use LIFF's api
-        initializeApp(data);
+        initializeApp();
     }).catch((err) => {
         console.log('error')
     });
@@ -53,31 +52,14 @@ window.onload = function (e) {
 // プロファイルの取得と表示
 function getProfile() {
     // https://developers.line.me/ja/reference/liff/#liffgetprofile()
-    liff.getProfile().then(function (profile) {
-        document.getElementById('useridprofilefield').textContent = profile.userId;
-        document.getElementById('displaynamefield').textContent = profile.displayName;
 
-        var profilePictureDiv = document.getElementById('profilepicturediv');
-        if (profilePictureDiv.firstElementChild) {
-            profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
-        }
-        var img = document.createElement('img');
-        img.src = profile.pictureUrl;
-        img.alt = "Profile Picture";
-        img.width = 200;
-        profilePictureDiv.appendChild(img);
-
-        document.getElementById('statusmessagefield').textContent = profile.statusMessage;
-    }).catch(function (error) {
-        window.alert("Error getting profile: " + error);
-    });
 }
 
 function initializeApp(data) {
-    document.getElementById('languagefield').textContent = data.language;
-    document.getElementById('viewtypefield').textContent = data.context.viewType;
-    document.getElementById('useridfield').textContent = data.context.userId;
-    document.getElementById('utouidfield').textContent = data.context.utouId;
-    document.getElementById('roomidfield').textContent = data.context.roomId;
-    document.getElementById('groupidfield').textContent = data.context.groupId;
+    document.getElementById('browserLanguage').textContent = liff.getLanguage();
+    document.getElementById('sdkVersion').textContent = liff.getVersion();
+    document.getElementById('lineVersion').textContent = liff.getLineVersion();
+    document.getElementById('isInClient').textContent = liff.isInClient();
+    document.getElementById('isLoggedIn').textContent = liff.isLoggedIn();
+    document.getElementById('deviceOS').textContent = liff.getOS();
 }
