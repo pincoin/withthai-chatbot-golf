b4d6ec6fb695bab00230123903a4804aa32c5f72
jonghwa <pincoins@gmail.com>
2020-08-10 22:38:51 +0900

    trailing slash

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'chatbot'
 
 urlpatterns = [
-    re_path(r'^(?P<slug>[-\w]+)/callback$',
+    re_path(r'^(?P<slug>[-\w]+)/callback/$',
             views.CallbackView.as_view(), name='callback'),
     re_path(r'^(?P<slug>[-\w]+)/liff/$',
             views.LiffView.as_view(), name='liff-index'),
