6949ef16be1bdd89bff09993ace7df937c43287c
jonghwa <pincoins@gmail.com>
2020-08-13 23:36:44 +0900

    delete and create

--- chatbot/management/commands/richmenu.py
+++ chatbot/management/commands/richmenu.py
@@ -26,6 +26,12 @@ class Command(BaseCommand):
 
         line_bot_api = linebot.LineBotApi(club.line_bot_channel_access_token)
 
+        # Delete all rich menus
+        rich_menu_list = line_bot_api.get_rich_menu_list()
+        for rich_menu in rich_menu_list:
+            print(rich_menu.rich_menu_id)
+            line_bot_api.delete_rich_menu(rich_menu.rich_menu_id)
+
         # Create rich menu
         rich_menu_to_create = models.RichMenu(
             size=models.RichMenuSize(width=2500, height=1686),
