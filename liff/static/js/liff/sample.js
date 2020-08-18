1933d84d192885616a8696d043b05e1c0329abf3
jonghwa <pincoins@gmail.com>
2020-08-19 03:09:41 +0900

    close button

--- liff/static/js/liff/sample.js
+++ liff/static/js/liff/sample.js
@@ -52,15 +52,6 @@ function registerButtonHandlers() {
         });
     });
 
-    // closeWindow call
-    document.getElementById('closeWindowButton').addEventListener('click', function () {
-        if (!liff.isInClient()) {
-            sendAlertIfNotInClient();
-        } else {
-            liff.closeWindow();
-        }
-    });
-
     // sendMessages call
     document.getElementById('sendMessageButton').addEventListener('click', function () {
         if (!liff.isInClient()) {
