fa83a480aee8cd1f7792294508e11f601ca855cf
jonghwa <pincoins@gmail.com>
2020-08-19 11:39:06 +0900

    save method

--- golf/models.py
+++ golf/models.py
@@ -266,15 +266,16 @@ class GolfClub(model_utils_models.TimeStampedModel):
         verbose_name_plural = _('Golf clubs')
 
     def save(self, *args, **kwargs):
-        self.info['header']['contents'][0]['text'] = self.title_english
-        self.info['hero']['action']['uri'] = self.website
-        self.info['body']['contents'][0]['contents'][1]['text'] = self.phone
-        self.info['body']['contents'][1]['contents'][1]['text'] = self.fax
-        self.info['body']['contents'][2]['contents'][1]['text'] = self.email
-        self.info['body']['contents'][3]['contents'][1]['text'] \
-            = '{} - {}'.format(time(self.business_hour_start, 'H:i'), time(self.business_hour_end, 'H:i'))
-        self.info['body']['contents'][4]['contents'][4]['action']['uri'] \
-            = f"https://liff.line.me/{self.liff['scorecard']['id']}"
+        if self.info and self.liff:
+            self.info['header']['contents'][0]['text'] = self.title_english
+            self.info['hero']['action']['uri'] = self.website
+            self.info['body']['contents'][0]['contents'][1]['text'] = self.phone
+            self.info['body']['contents'][1]['contents'][1]['text'] = self.fax
+            self.info['body']['contents'][2]['contents'][1]['text'] = self.email
+            self.info['body']['contents'][3]['contents'][1]['text'] \
+                = '{} - {}'.format(time(self.business_hour_start, 'H:i'), time(self.business_hour_end, 'H:i'))
+            self.info['body']['contents'][4]['contents'][4]['action']['uri'] \
+                = f"https://liff.line.me/{self.liff['scorecard']['id']}"
 
         super(GolfClub, self).save(*args, **kwargs)
 
