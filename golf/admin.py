cd887397ba56ddc1e4a4bfb03fb194a57b40f339
jonghwa <pincoins@gmail.com>
2020-08-29 19:36:57 +0900

    weekend booking on monday

--- golf/admin.py
+++ golf/admin.py
@@ -63,7 +63,7 @@ class GolfClubAdmin(admin.ModelAdmin):
                        'phone', 'email', 'fax', 'website', 'address', 'latitude', 'longitude', 'district',
                        'caddie_compulsory', 'cart_compulsory', 'min_pax', 'max_pax', 'customer_group',
                        'weekdays_min_in_advance', 'weekdays_max_in_advance',
-                       'weekend_min_in_advance', 'weekend_max_in_advance')
+                       'weekend_min_in_advance', 'weekend_max_in_advance', 'weekend_booking_on_monday')
         }),
         ('LINE', {
             'fields': ('line_bot_channel_access_token', 'line_bot_channel_secret', 'line_notify_access_token',
