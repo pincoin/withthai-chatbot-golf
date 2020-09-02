c04b991fc2f7c7403ff08b24c984f03e21cd511f
jonghwa <pincoins@gmail.com>
2020-09-03 01:52:08 +0900

    path

--- console/urls.py
+++ console/urls.py
@@ -1,16 +1,18 @@
-from django.urls import re_path
+from django.urls import (
+    re_path, path
+)
 
 from . import views
 
 app_name = 'console'
 
 urlpatterns = [
-    re_path(r'',
-            views.HomeView.as_view(), name='home'),
+    path('',
+         views.HomeView.as_view(), name='home'),
 
-    re_path(r'(?P<slug>[-\w]+)/orders/',
+    re_path(r'^(?P<slug>[-\w]+)/orders/$',
             views.GolfBookingOrderListView.as_view(), name='golf-booking-order-list'),
 
-    re_path(r'(?P<slug>[-\w]+)/orders/<uuid:uuid>/',
+    re_path(r'^(?P<slug>[-\w]+)/orders/<uuid:uuid>/$',
             views.GolfBookingOrderDetailView.as_view(), name='golf-booking-order-detail'),
 ]
