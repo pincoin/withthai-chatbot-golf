3f4b65edef33171fd4e2ab9f88846a7769dd5f00
jonghwa <pincoins@gmail.com>
2020-09-04 03:14:42 +0900

    web fonts

--- console/views.py
+++ console/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.views import generic
 
 from golf import models as golf_models
@@ -30,8 +31,18 @@ class GolfBookingOrderListView(EnglishContextMixin, generic.ListView):
         return context
 
 
-class GolfBookingOrderDetailView(generic.TemplateView):
+class GolfBookingOrderDetailView(EnglishContextMixin, generic.DetailView):
     template_name = 'console/golf_booking_order_detail.html'
+
+    context_object_name = 'order'
+
+    def get_object(self, queryset=None):
+        # NOTE: This method is overridden because DetailView must be called with either an object pk or a slug.
+        queryset = golf_models.GolfBookingOrder.objects \
+            .select_related('customer_group', 'golf_club') \
+            .filter(order_no=self.kwargs['uuid'])
+
+        return get_object_or_404(queryset, order_no=self.kwargs['uuid'])
 
     def get_context_data(self, **kwargs):
         context = super(GolfBookingOrderDetailView, self).get_context_data(**kwargs)
