e2c492a3565acafe29ec16b6726c56efdaf745c5
jonghwa <pincoins@gmail.com>
2020-09-08 17:51:23 +0900

    set search default

--- console/static/js/console/order-list.js
+++ console/static/js/console/order-list.js
@@ -1,21 +1,25 @@
+function setSearchKeyword(search, keyword) {
+    if (search.value === 'round_date') {
+        keyword.type = 'date';
+        keyword.value = '';
+        keyword.placeholder = 'HH:MM';
+    } else if (search.value === 'customer') {
+        keyword.type = 'text';
+        keyword.value = '';
+        keyword.placeholder = 'Customer name';
+    }
+}
+
 function runApp() {
     const search = document.getElementById('id_search');
     const keyword = document.getElementById('id_keyword');
 
-    keyword.type = 'date';
+    setSearchKeyword(search, keyword);
 
     search.addEventListener('change', function (e) {
         console.log('changed');
         console.log(this.value);
 
-        if (this.value === 'round_date') {
-            keyword.type = 'date';
-            keyword.value = '';
-            keyword.placeholder = 'HH:MM';
-        } else if (this.value === 'customer') {
-            keyword.type = 'text';
-            keyword.value = '';
-            keyword.placeholder = 'Customer name';
-        }
+        setSearchKeyword(search, keyword);
     });
 }
