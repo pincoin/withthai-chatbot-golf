5fc0be470a10e7507bc0ea8e0417b0580fd342d1
jonghwa <pincoins@gmail.com>
2020-09-09 22:54:36 +0900

    auto submit

---
+++ console/static/js/console/green-fee-list.js
@@ -0,0 +1,12 @@
+function runApp() {
+    const seasons = document.getElementById('id_seasons');
+    const day_of_week = document.getElementById('id_day_of_week');
+    const timeslots = document.getElementById('id_timeslots');
+    const customer_groups = document.getElementById('id_customer_groups');
+
+    [seasons, day_of_week, timeslots, customer_groups].forEach(function (element) {
+        element.addEventListener('change', function (e) {
+             this.form.submit();
+        });
+    });
+}
