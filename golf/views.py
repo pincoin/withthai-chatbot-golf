91ca30cd109dca83d3181ec535e34247fe10b805
jonghwa <pincoins@gmail.com>
2020-09-09 10:45:54 +0900

    exception handling

--- golf/views.py
+++ golf/views.py
@@ -36,16 +36,19 @@ class GolfClubCustomerGroup(generic.TemplateView):
     def render_to_response(self, context, **response_kwargs):
         profile = get_profile(self.request.GET['access_token'])
 
-        try:
-            membership = models.LineUserMembership \
-                .objects.get(line_user__line_user_id=profile['userId'],
-                             customer_group__golf_club__slug=self.kwargs['slug'])
-            data = {
-                'customer_group_id': membership.customer_group_id,
-                'customer_group_title_english': membership.customer_group.title_english,
-            }
-        except models.LineUserMembership.DoesNotExist:
-            data = {}
+        data = {}
+
+        if profile:
+            try:
+                membership = models.LineUserMembership \
+                    .objects.get(line_user__line_user_id=profile['userId'],
+                                 customer_group__golf_club__slug=self.kwargs['slug'])
+                data = {
+                    'customer_group_id': membership.customer_group_id,
+                    'customer_group_title_english': membership.customer_group.title_english,
+                }
+            except models.LineUserMembership.DoesNotExist:
+                pass
 
         return JsonResponse(
             data,
