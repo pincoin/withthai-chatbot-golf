e76c8995fa2087a72fe378e23e9361bb172443d6
jonghwa <pincoins@gmail.com>
2020-09-15 02:45:40 +0900

    burger make it later

--- console/static/js/console/main.js
+++ console/static/js/console/main.js
@@ -1,15 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document
-        .getElementById('burger-icon')
-        .addEventListener('click', function (e) {
-            const left_sidebar = document.getElementById('left-sidebar');
-
-            if (left_sidebar.classList.contains('is-hidden-mobile')) {
-                left_sidebar.classList.remove('is-hidden-mobile');
-            } else {
-                left_sidebar.classList.add('is-hidden-mobile');
-            }
-        });
-
     runApp();
 });
