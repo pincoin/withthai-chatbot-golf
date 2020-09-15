b9ad101173332360bf0dadb765bbb0b119450feb
jonghwa <pincoins@gmail.com>
2020-09-15 23:26:02 +0900

    force language

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'conf.middleware.ForceLangMiddleware',
 ]
 
 ROOT_URLCONF = 'conf.urls'
