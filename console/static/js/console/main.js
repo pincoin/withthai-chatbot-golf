80e51d82ba30d9a5909e36f3e4f2d2dd929e8075
jonghwa <pincoins@gmail.com>
2020-09-15 10:55:58 +0900

    navbar top box shadow

--- console/static/js/console/main.js
+++ console/static/js/console/main.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    const navbarTop = document.getElementById('navbar-top');
+
+    window.addEventListener('scroll', function (e) {
+        if ((window.scrollY || document.documentElement.scrollTop) > 20) {
+            navbarTop.style.boxShadow = "0 10px 5px -2px rgba(0,0,0,0.2)";
+            navbarTop.style.transitionDuration = "0.1s";
+        } else {
+            navbarTop.style.boxShadow = "none";
+            navbarTop.style.transitionDuration = "inherit";
+        }
+    });
+
     runApp();
 });
