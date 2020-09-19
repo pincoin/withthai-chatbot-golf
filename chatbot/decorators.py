8ef5c276c2d6d09212546d6d17d6497c4349995d
jonghwa <pincoins@gmail.com>
2020-09-19 22:01:30 +0900

    language

--- chatbot/decorators.py
+++ chatbot/decorators.py
@@ -6,7 +6,7 @@ from django.utils import translation
 def translation_activate(func):
     @wraps(func)
     def decorated(*args, **kwargs):
-        translation.activate('en')
+        translation.activate(kwargs['membership'].line_user.lang)
         func(*args, **kwargs)
         translation.deactivate()
 
