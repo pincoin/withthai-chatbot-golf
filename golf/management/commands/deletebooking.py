9fd3d82117bd844b5d491f4709725d2c39dd3a79
jonghwa <pincoins@gmail.com>
2020-09-26 03:02:44 +0900

    delete booking

---
+++ golf/management/commands/deletebooking.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from golf import models
+
+
+class Command(BaseCommand):
+    help = 'Delete booking'
+
+    '''
+    python manage.py deletebooking
+    '''
+
+    def handle(self, *args, **options):
+        models.GolfBookingOrder.objects \
+            .select_related('line_user') \
+            .filter(order_status=models.GolfBookingOrder.PAYMENT_STATUS_CHOICES.unpaid,
+                    line_user__follow_status=models.LineUser.FOLLOW_CHOICES.unfollow,
+                    round_date__lt=timezone.make_aware(timezone.localtime().today())) \
+            .delete()
+
+        self.stdout.write(self.style.SUCCESS('Delete booking'))
