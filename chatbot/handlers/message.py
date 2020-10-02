5b6cb35989930f192eb4ec74925c2db8efdeb553
jonghwa <pincoins@gmail.com>
2020-10-02 23:00:51 +0900

    margin fix

--- chatbot/handlers/message.py
+++ chatbot/handlers/message.py
@@ -279,7 +279,7 @@ def command_booking(event, line_bot_api, **kwargs):
                         'style': 'primary',
                         'height': 'sm',
                         'color': '#00acc1',
-                        'margin': 'none'
+                        'margin': 'sm'
                     }
                 ]
             }
