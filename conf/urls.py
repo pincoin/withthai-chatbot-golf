779a52d61ef2fa6d5da8e3f3fbbc1c6a7397de03
jonghwa <pincoins@gmail.com>
2020-10-19 07:29:12 +0900

    custom error pages

--- conf/urls.py
+++ conf/urls.py
@@ -35,3 +35,7 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+handler404 = 'conf.views.handler404'
+handler403 = 'conf.views.handler403'
+handler500 = 'conf.views.handler500'
--- conf/views.py
+++ conf/views.py
@@ -1,5 +1,27 @@
+from django.shortcuts import render
 from django.views import generic
 
 
 class HomeView(generic.TemplateView):
     template_name = 'conf/home.html'
+
+
+def handler404(request, exception):
+    # Page not found
+    response = render(request, '404.html', {})
+    response.status_code = 404
+    return response
+
+
+def handler403(request, exception=None):
+    # Permission denied
+    response = render(request, '403.html', {})
+    response.status_code = 403
+    return response
+
+
+def handler500(request, exception=None):
+    # Internal server error
+    response = render(request, '500.html', {})
+    response.status_code = 500
+    return response
